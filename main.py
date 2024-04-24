6bcffc7cecc38081ae837671e46df1cc95ddcf1a
unknown <Berik>
2024-04-24 10:17:57 +0600

    update

---
+++ main.py
@@ -0,0 +1,34 @@
+# Код для aiogram
+
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+# Объект бота. Вставьте сюда свой токен
+bot = Bot(token="6981278189:AAFhhbvjNc6YNzhQz31JijZCalKAKYJx5OQ")
+
+# Диспетчер
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="С пюрешкой")],
+        [types.KeyboardButton(text="Без пюрешки")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
